fcb43b428072415de87c2c4e3f5454bfb28713b4
Gonzalo Campos <erick.campos@wizeline.com>
2020-05-23 23:11:16 -0500

    Create StudentDao

---
+++ src/main/java/com/gonz/mx/spring/boot/spike/dao/StudentDao.java
@@ -0,0 +1,32 @@
+package com.gonz.mx.spring.boot.spike.dao;
+
+import com.gonz.mx.spring.boot.spike.entity.Student;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+// This is the data layer.
+// It will maintain the connection with the data sources.
+// In this case, we are using fake data.
+
+@Repository
+public class StudentDao {
+
+    private static Map<Integer, Student> students;
+
+    static {
+        students = new HashMap<Integer, Student>() {
+            {
+                put(1, new Student(1, "Abcd", "Abcd"));
+                put(2, new Student(2, "Abcd", "Abcd"));
+                put(3, new Student(3, "Abcd", "Abcd"));
+            }
+        };
+    }
+
+    public Collection<Student> getAllStudents() {
+        return students.values();
+    }
+}
