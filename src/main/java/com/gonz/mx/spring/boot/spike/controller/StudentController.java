3015f6cb60e981d6c6fc5d95e3ce3cbf798c7550
Gonzalo Campos <erick.campos@wizeline.com>
2020-05-23 23:21:50 -0500

    Add @RequestMapping annotation to our controller

--- src/main/java/com/gonz/mx/spring/boot/spike/controller/StudentController.java
+++ src/main/java/com/gonz/mx/spring/boot/spike/controller/StudentController.java
@@ -3,17 +3,32 @@ package com.gonz.mx.spring.boot.spike.controller;
 import com.gonz.mx.spring.boot.spike.entity.Student;
 import com.gonz.mx.spring.boot.spike.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
 // The controller will attend the client requests (REST)
+// The @RequestMapping annotation will define
+// the endpoint of the controller.
+
+// The endpoint can also be defined at a method level,
+// but in this case it's not needed since we are defining
+// the endpoint at a class level.
+
+// Something like:
+// @RequestMapping(value = "/endpoint", method = RequestMethod.GET)
+// public Collection<Student> getAllStudents() ...
+
 @RestController
+@RequestMapping("/students") // URL
 public class StudentController {
 
     @Autowired
     private StudentService studentService;
 
+    @RequestMapping(method = RequestMethod.GET)
     public Collection<Student> getAllStudents() {
         return studentService.getAllStudents();
     }
